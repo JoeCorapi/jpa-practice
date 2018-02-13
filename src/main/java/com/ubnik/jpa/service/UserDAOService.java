bb75651bd834532c275877a212dcd227a0a4fa41
Joe Corapi <24542019+JoeCorapi@users.noreply.github.com>
2018-02-13 16:56:58 -0500

    Create DAO Service

---
+++ src/main/java/com/ubnik/jpa/service/UserDAOService.java
@@ -0,0 +1,24 @@
+package com.ubnik.jpa.service;
+
+import com.ubnik.jpa.jpapractice.entity.User;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+@Repository
+@Transactional
+public class UserDAOService {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public long insert(User user){
+        //Open transaction
+        entityManager.persist(user);
+
+        //Close transaction
+        return user.getId();
+    }
+}
