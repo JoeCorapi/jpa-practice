260e2a0c302f963f5275f494792d4c473440aa38
Joe Corapi <24542019+JoeCorapi@users.noreply.github.com>
2018-02-16 23:15:27 -0500

    Posts repository for JPA

--- rest-jpa-integration/src/main/resources/data.sql
+++ rest-jpa-integration/src/main/resources/data.sql
@@ -1,3 +1,6 @@
-INSERT into user VALUES(100001, SYSDATE, 'AB');
-INSERT into user VALUES(100002, SYSDATE, 'Jill');
-INSERT into user VALUES(100003, SYSDATE, 'Jam');
+INSERT into user VALUES(10001, SYSDATE, 'AB');
+INSERT into user VALUES(10002, SYSDATE, 'Jill');
+INSERT into user VALUES(10003, SYSDATE, 'Jam');
+
+INSERT into user VALUES(11001, 'My First Post', 10001);
+INSERT into user VALUES(11003, 'My Second Post', 10001);
