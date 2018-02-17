df55c458ae72ec0e0716c9f0a06d7ca2363c4e7b
Joe Corapi <24542019+JoeCorapi@users.noreply.github.com>
2018-02-16 21:34:41 -0500

    temp fix to avoid user ID collision

--- rest-jpa-integration/src/main/resources/data.sql
+++ rest-jpa-integration/src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT into user VALUES(1, SYSDATE, 'AB');
-INSERT into user VALUES(2, SYSDATE, 'Jill');
-INSERT into user VALUES(3, SYSDATE, 'Jam');
+INSERT into user VALUES(100001, SYSDATE, 'AB');
+INSERT into user VALUES(100002, SYSDATE, 'Jill');
+INSERT into user VALUES(100003, SYSDATE, 'Jam');
